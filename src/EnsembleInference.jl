869de100e3d5dacd31b588e622f17582fe683331
Seth Axen <seth.axen@gmail.com>
2020-11-06 22:02:50 -0800

    Add static reshaper for SE3

--- src/EnsembleInference.jl
+++ src/EnsembleInference.jl
@@ -5,4 +5,13 @@ using Manifolds: Manifolds, SpecialEuclidean, SpecialOrthogonal, TranslationGrou
 using Random
 using Statistics: Statistics
 
+const SHAPESPEC_SE3 = Manifolds.ShapeSpecification(
+    Manifolds.StaticReshaper(),
+    Manifolds.base_manifold(SpecialEuclidean(3)).manifolds...,
+)
+
+se3_array(x) = Manifolds.ProductArray(SHAPESPEC_SE3, x)
+
+export se3_array
+
 end # module
