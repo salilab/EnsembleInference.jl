846fdea18778c1f1e6f9feae7dc7c19de027850a
Seth Axen <seth.axen@gmail.com>
2021-01-30 23:14:15 -0800

    Add SO(3) and so(3) representations (#5)
    * Stop formatting docstrings
    
    * Add Kronecker delta function
    
    * Add alias for groups and algebras
    
    * Add aliases for basis vectors
    
    * Add SO(3) representations
    
    * Test SO(3) representations
    
    * Fix typo
    
    * Test half-integral blocks as well
    
    * Correctly specify iteration
    
    * Reformat

--- src/EnsembleInference.jl
+++ src/EnsembleInference.jl
@@ -8,6 +8,31 @@ using Random
 using Statistics: Statistics
 using StatsBase: StatsBase
 
+const SO3 = SpecialOrthogonal(3)
+const SE3 = SpecialEuclidean(3)
+
+# this type is defined to avoid Manifolds's complicated type vector space type hierarchy and
+# to avoid choosing a point representation
+struct LieAlgebra{G}
+    group::G
+end
+
+const so3 = LieAlgebra(SO3)
+const se3 = LieAlgebra(SE3)
+
+struct BasisVector{B,I}
+    basis::B
+end
+BasisVector(basis, i::Integer) = BasisVector{typeof(basis),i}(basis)
+
+# basis vectors needed for SO(3) (E1-E3) and SE(3) (E1-E6)
+const E1 = BasisVector(Manifolds.DefaultOrthogonalBasis(), 1)
+const E2 = BasisVector(Manifolds.DefaultOrthogonalBasis(), 2)
+const E3 = BasisVector(Manifolds.DefaultOrthogonalBasis(), 3)
+const E4 = BasisVector(Manifolds.DefaultOrthogonalBasis(), 4)
+const E5 = BasisVector(Manifolds.DefaultOrthogonalBasis(), 5)
+const E6 = BasisVector(Manifolds.DefaultOrthogonalBasis(), 6)
+
 const SHAPESPEC_SE3 = Manifolds.ShapeSpecification(
     Manifolds.StaticReshaper(), Manifolds.base_manifold(SpecialEuclidean(3)).manifolds...
 )
@@ -26,7 +51,9 @@ include("utility.jl")
 include("dirac.jl")
 include("haar.jl")
 include("diffusion_normal.jl")
+include("representation.jl")
 
+export SO3, so3, SE3, se3
 export DiffusionNormal, DiracDelta, Haar
 export inversion, se3_array
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using EnsembleInference
 using Test
 
 @testset "EnsembleInference.jl" begin
+    include("utility.jl")
     include("dirac.jl")
     include("haar.jl")
     include("diffusion_normal.jl")
+    include("representation.jl")
 end
