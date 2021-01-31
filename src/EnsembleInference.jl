503828ce3c89c7f8b81d8423dda3ff1a9e9e0a10
Seth Axen <seth.axen@gmail.com>
2021-01-30 22:55:38 -0800

    Add alias for groups and algebras

--- src/EnsembleInference.jl
+++ src/EnsembleInference.jl
@@ -8,6 +8,18 @@ using Random
 using Statistics: Statistics
 using StatsBase: StatsBase
 
+const SO3 = SpecialOrthogonal(3)
+const SE3 = SpecialEuclidean(3)
+
+# this type is defined to avoid Manifolds's complicated type vector space type hierarchy and
+# to avoid choosing a point representation
+struct LieAlgebra{G}
+    group::G
+end
+
+const so3 = LieAlgebra(SO3)
+const se3 = LieAlgebra(SE3)
+
 const SHAPESPEC_SE3 = Manifolds.ShapeSpecification(
     Manifolds.StaticReshaper(), Manifolds.base_manifold(SpecialEuclidean(3)).manifolds...
 )
@@ -27,6 +39,7 @@ include("dirac.jl")
 include("haar.jl")
 include("diffusion_normal.jl")
 
+export SO3, so3, SE3, se3
 export DiffusionNormal, DiracDelta, Haar
 export inversion, se3_array
 
